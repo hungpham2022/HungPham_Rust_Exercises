e2f03f0d1e8a5fd076c4b6f129155d95e43ffb45
Hung Pham <phamvanhung11@gmail.com>
2022-06-10 16:14:52 +0700

    hello cargo done

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\Learning\Code\VBI_Rust_Subtrate\HungPham_Rust_Exercises\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\Learning\Code\VBI_Rust_Subtrate\HungPham_Rust_Exercises\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\Learning\Code\VBI_Rust_Subtrate\HungPham_Rust_Exercises\hello_cargo\target\debug\hello_cargo.exe: E:\Learning\Code\VBI_Rust_Subtrate\HungPham_Rust_Exercises\hello_cargo\src\main.rs
